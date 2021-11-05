32be82ab278f5d8b28000659ed96acb368735d77
ChrisPag <chrisp99@my.yorku.ca>
2021-11-05 16:22:11 -0400

    Added text box
    Added text box that appears onclick

--- src/Create.js
+++ src/Create.js
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 
+
 const Create = () => {
     const [desc, setDesc] = useState();
+    const [showTask, setShowTask] = useState(false);
     
     const handleClick = () => {
-        setDesc('eat food');
+        setShowTask(true)
     }
 
     return (
         <div className = "create">
             <button onClick={handleClick}>Add Task</button>
-            <div className ="tasks">
-                <p>{desc}</p>
-            </div>
+            {showTask && 
+            <form>
+                <input
+                    type="text"
+                    placeholder="Enter task here"
+                    value={ desc }
+                    onChange = {(e) => setDesc(e.target.value)}
+                />
+            </form>}
             
         </div>
     );
